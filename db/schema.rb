8b6a20cac78e5c8f8bdd4ef915e154d2f807e867
Blandine <blandine.kaneza@gmail.com>
2018-12-05 12:44:43 +0200

    Setup the app with database

---
+++ db/schema.rb
@@ -0,0 +1,53 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2018_12_05_104226) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "missions", force: :cascade do |t|
+    t.text "mission_name"
+    t.text "mission_id"
+    t.text "wikipedia"
+    t.text "website"
+    t.text "twitter"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "nationalities", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "payloads", force: :cascade do |t|
+    t.text "payload_id"
+    t.boolean "reused"
+    t.text "manufacturer"
+    t.text "payload_type"
+    t.float "payload_mass_kg"
+    t.float "payload_mass_lbs"
+    t.text "orbit"
+    t.bigint "mission_id"
+    t.bigint "nationality_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mission_id"], name: "index_payloads_on_mission_id"
+    t.index ["nationality_id"], name: "index_payloads_on_nationality_id"
+  end
+
+  add_foreign_key "payloads", "missions"
+  add_foreign_key "payloads", "nationalities"
+end
