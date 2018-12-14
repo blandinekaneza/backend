57bc4c23b1756271657c33dd987444f344f52faf
Blandine <blandine.kaneza@gmail.com>
2018-12-14 11:15:04 +0200

    modify missions relationship routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 	jsonapi_resources :fetch_data
 	jsonapi_resources :missions do
 		get 'search', on: :collection
+		jsonapi_related_resources :payloads
+		jsonapi_related_resources :nationalities
 	end
 	jsonapi_resources :payloads
 	jsonapi_resources :nationalities
